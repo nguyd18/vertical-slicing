2975e63543e02662fb9200786c44b08965d43040
Daniel Nguyen <nguyd18@mcmaster.ca>
2025-01-17 11:29:32 -0500

    added Quebec functionality (close #2)

--- mcmerch.py
+++ mcmerch.py
@@ -3,12 +3,18 @@ def main():
     # configure first cli arg to be the price of the time
     parser = argparse.ArgumentParser()
     parser.add_argument("price", type=float, help="The price of the item")
+    parser.add_argument("--province", type=str, default="Ontario", choices=["Ontario", "Quebec"], help="The province where the tax is applied (default: Ontario)")
     args = parser.parse_args()
-    price = args.price
 
     # add tax to the price
-    total = price * 1.13 #since in Ontario
-
+    if args.province == "Ontario":
+        tax_rate = 1.13
+    elif args.province == "Quebec":
+        tax_rate = 1.14975
+    
+    total = args.price * tax_rate
+    
     print(f"The total price is {total:.2f}")
+
 if __name__ == '__main__':
     main()
